277f662819ffab09231f496aa60101b4a9bb7c9f
hemantadongol22 <hemantadongol11@gmail.com>
2022-01-05 17:35:15 +0545

    Initial

---
+++ Database/admission.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 05, 2022 at 12:27 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.3.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `admission`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_data`
+--
+
+CREATE TABLE `student_data` (
+  `id` int(10) NOT NULL,
+  `u_card` varchar(12) NOT NULL,
+  `u_f_name` text NOT NULL,
+  `u_l_name` text NOT NULL,
+  `u_father` text NOT NULL,
+  `u_aadhar` varchar(12) NOT NULL,
+  `u_birthday` text NOT NULL,
+  `u_gender` varchar(6) NOT NULL,
+  `u_email` text NOT NULL,
+  `u_phone` varchar(10) NOT NULL,
+  `u_state` varchar(12) NOT NULL,
+  `u_dist` text NOT NULL,
+  `u_village` text NOT NULL,
+  `u_pincode` text NOT NULL,
+  `file` longblob NOT NULL,
+  `u_mother` varchar(30) NOT NULL,
+  `u_family` text NOT NULL,
+  `staff_id` varchar(12) NOT NULL,
+  `image` varchar(150) NOT NULL,
+  `uploaded` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student_data`
+--
+
+INSERT INTO `student_data` (`id`, `u_card`, `u_f_name`, `u_l_name`, `u_father`, `u_aadhar`, `u_birthday`, `u_gender`, `u_email`, `u_phone`, `u_state`, `u_dist`, `u_village`, `u_pincode`, `file`, `u_mother`, `u_family`, `staff_id`, `image`, `uploaded`) VALUES
+(127, '963', 'Hemanta', 'Dongol', 'Hera Kaji Dongol', '12', '2021-09-01', 'Male', 'hemantadongol22@gmail.com', '9840093343', 'Province 3', 'Kathmandu', 'Swayambhu', '', '', 'Purna Laxmi Dongol', '', '1', '255837591_4266089970186585_5836718013735614058_n.jpg', '2022-01-03 18:13:58'),
+(128, '975', 'Pramesh', 'Mhrzn', 'Prem Bahadur Mhrzn', '123', '2021-10-14', 'Female', 'mhrzp8@gmail.com', '9840093343', 'Province 3', 'Kathmandu', 'Dakshinkali', '', '', 'Sanu Maya Mhrzn', '', '1', '270191438_4430885597040354_3923519290243767830_n.jpg', '2022-01-03 18:28:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
+(8, 'admin', '$2y$10$slSgzqMzon.hmj3DTmZe9e52SJqWOBeXrUJmIEMcJgPJ/DbXVCcEW', '2022-01-03 17:38:32');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student_data`
+--
+ALTER TABLE `student_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student_data`
+--
+ALTER TABLE `student_data`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=129;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
